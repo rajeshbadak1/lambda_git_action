3c51b9ca1c78d689a9b7a6cbcf92f7403526a759
Rajesh <rajesh.badak@coindelta.io>
2022-06-24 14:23:26 +0530

    test

--- lambda.py
+++ lambda.py
@@ -1,8 +1,9 @@
 import json
 
 def lambda_handler(event, context):
+    
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Rajesh Hello from Lambda!')
     }
